ca53e74f9c303fd6496c518dda2ff8a68b797f36
Ron-Arjay Caluag <arjaycaluag@gmail.com>
2021-02-18 16:08:54 -0800

    Create workout schema

---
+++ public/models/workout.js
@@ -0,0 +1,49 @@
+// Require mongoose
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema({
+  day: {
+    type: Date,
+    default: () => new Date(),
+  },
+
+  exercises: [
+    {
+      type: {
+        type: String,
+        trim: true,
+        required: "Must enter exercise type",
+      },
+      name: {
+        type: String,
+        trim: true,
+        required: "Must enter exercise name",
+      },
+      duration: {
+        type: Number,
+        required: "Must Enter minute duration",
+      },
+      weight: {
+        type: Number,
+        required: "Enter amount of weights used",
+      },
+      sets: {
+        type: Number,
+        required: "Enter amount of sets",
+      },
+      distance: {
+        type: Number,
+        required: "Enter amount of miles ran",
+      },
+    },
+  ],
+  totalDuration: {
+      type: Number,
+      default: 0,
+  }
+});
+
+const Workout = mongoose.model("Workout", workoutSchema);
+module.exports = Workout;
