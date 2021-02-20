128fa608b8a419d4a135af4a578688e4b1e372ab
Ron-Arjay Caluag <arjaycaluag@gmail.com>
2021-02-19 20:21:44 -0800

    Create workout route

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -13,4 +13,15 @@ router.get("/api/workouts", (req, res) => {
     });
 });
 
+// Create workout
+router.post("/api/workouts", ({ body }, res) => {
+
+  db.Workout.create(body).then((dbWorkout => {
+      res.json(dbWorkout);
+  })).catch(err => {
+      res.json(err);
+  });
+});
+
+
 module.exports = router;
