8bca40a88362d33366e1d9a1f72afe1fc8768b96
Ron-Arjay Caluag <arjaycaluag@gmail.com>
2021-02-19 16:59:49 -0800

    Move model out of public folder and create find all workout api route

---
+++ routes/api-routes.js
@@ -0,0 +1,16 @@
+// Require our models folder and express router
+const db = require("../models");
+const router = require("express").Router();
+
+// Find all workouts
+router.get("/api/workouts", (req, res) => {
+  db.Workout.find({})
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -16,8 +16,8 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
 });
 
 // routes
-app.use(require("./routes/api-routes"));
-app.use(require("./routes/html-routes"));
+app.use(require("./routes/api-routes.js"));
+app.use(require("./routes/html-routes.js"));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
