31faebd767e4486edd9f7a4a91d490e256109f05
Ron-Arjay Caluag <arjaycaluag@gmail.com>
2021-02-19 20:45:39 -0800

    Create work outs in range route for dashboard

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -35,4 +35,15 @@ router.put("/api/workouts/:id", ({ body, params }, res) => {
     });
 });
 
+
+router.get("/api/workouts/range", (req, res) => {
+  db.Workout.find({})
+  .then(dbWorkout => {
+    res.json(dbWorkout);
+  })
+  .catch (err => {
+    res.json(err);
+  });
+});
+
 module.exports = router;
